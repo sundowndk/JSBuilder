ddc0a35d862319a4b5de0fe3be810c37a6fa2205
Rasmus Pedersen <rasmus@akvaservice.dk>
2011-10-05 12:40:40 +0200

    import

---
+++ Source/JSBuilder/Constructor.cs
@@ -0,0 +1,15 @@
+using System;
+
+using System.IO;
+using System.Xml;
+
+namespace JSBuilder2
+{
+	public class Constructor
+	{
+		public Constructor ()
+		{
+		}
+	}
+}
+
---
+++ Source/JSBuilder/Main.cs
@@ -0,0 +1,35 @@
+using System;
+
+using System.IO;
+using System.Xml;
+namespace JSBuilder2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+			
+			if (args.Length > 0)
+			{		
+				string path = string.Empty;
+				if (!System.IO.Path.IsPathRooted (args[0]))
+				{
+					path = System.Environment.CurrentDirectory + "/" + args[0];
+				}
+				else
+				{
+					path = args[0];
+				}
+				
+				Solution solution = Solution.Load (path);
+				solution.Build ();				
+			}
+			else
+			{
+				Console.WriteLine ("Syntax: inputfile");
+			}
+		}				
+	}
+}
+
